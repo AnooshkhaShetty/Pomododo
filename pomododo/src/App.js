5a038643df575c74244b332916dd11858464d11a
AnooshkhaShetty <anooshkhashetty@gmail.com>
2023-04-21 21:22:40 -0700

    Squashed commit of the following:
    commit e010d92ee469ffebbf31ea294ff7e3a0fccf349e
    Author: AnooshkhaShetty <anooshkhashetty@gmail.com>
    Date:   Fri Apr 21 20:57:33 2023 -0700
    
        created a simple countdown

--- pomododo/src/App.js
+++ pomododo/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [timeRemaining, setTimeRemaining] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const startTimer = () => {
+    setTimeRemaining(duration * 60);
+  };
+
+  useEffect(() => {
+    let interval;
+    if (timeRemaining > 0) {
+      interval = setInterval(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [timeRemaining]);
+
+  const handleDurationChange = (event) => {
+    setDuration(event.target.value);
+  };
+
+  const minutes = Math.floor(timeRemaining / 60);
+  const seconds = timeRemaining % 60;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1>Pomododo</h1>
+      <input type="number" value={duration} onChange={handleDurationChange} />
+      <button onClick={startTimer}>Start Timer</button>
+      <p>
+        Time Remaining: {minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}
+      </p>
     </div>
   );
 }
 
-export default App;
+export default Timer;
