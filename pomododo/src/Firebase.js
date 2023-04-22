1521a11cda6c6687591ae5bd761b46a71cf0936d
Ashish-Basetty <ashish.basetty2@gmail.com>
2023-04-22 13:59:38 -0700

    added login page

--- pomododo/src/Firebase.js
+++ pomododo/src/Firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +17,19 @@ const firebaseConfig = {
   measurementId: "G-30HYP50JEB"
 };
 
-// Initialize Firebas
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app)
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider).then((result) => {
+    const name = result.user.displayName
+    const photoURL = result.user.photoURL
+
+    localStorage.setItem("name", name)
+    localStorage.setItem("photoURL", photoURL)
+  })
+}
