16f89d64d33ef962e641316058edbaf72fcb3278
Felix Qu <fequ830@gmail.com>
2023-04-22 12:42:41 -0700

    added homepage router

--- pomododo/src/pages/Routes.js
+++ pomododo/src/pages/Routes.js
@@ -2,12 +2,14 @@ import * as React from 'react'
 import { Route, Routes } from "react-router-dom";
 
 import FocusPage from "./FocusPage"
+import HomePage from "./HomePage"
 
 const Router = () => {
 
   return (
     <Routes>
       <Route exact path="/timetofocus" element={<FocusPage />} />
+      <Route exact path="/selectmethod" element={<HomePage />} />
     </Routes>
   );
 }
