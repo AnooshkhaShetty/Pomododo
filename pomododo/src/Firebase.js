4642c24085c9506795b97d9fb80daa7dd4c2304e
Ashish-Basetty <ashish.basetty2@gmail.com>
2023-04-22 13:57:19 -0700

    test

--- pomododo/src/Firebase.js
+++ pomododo/src/Firebase.js
@@ -16,6 +16,6 @@ const firebaseConfig = {
   measurementId: "G-30HYP50JEB"
 };
 
-// Initialize Firebase
+// Initialize Firebas
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
