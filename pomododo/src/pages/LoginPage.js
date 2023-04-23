7f356ac9f393ec27e7b6387e830d644a8c44419a
michaeljiang21 <110742454+mcj8@users.noreply.github.com>
2023-04-22 19:47:28 -0700

    Added logout feature to homepage and fixed Login bug.
    Created a LogoutButton function in Firebase.js and included it in HomePage.js. Also added conditional that prevents users from accessing homepage without being logged in.

--- pomododo/src/pages/LoginPage.js
+++ pomododo/src/pages/LoginPage.js
@@ -1,12 +1,22 @@
-import { signInWithGoogle } from "../Firebase"
+import { auth, signInWithGoogle } from "../Firebase"
 import { useNavigate } from 'react-router-dom'
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function Login(){
+
+    const [user, loading, error] = useAuthState(auth);
     const navigate = useNavigate();
+    
+    //If the user exists, navigate to the homepage
+    if (user) {
+        navigate("/selectmethod");
+        return null;
+    }
+
     return (
         <div className="Login">
             <h1> Welcome to Pomododo</h1>
-            <button onClick={() =>{signInWithGoogle(); navigate("/selectmethod");}}> Login </button>
+            <button onClick={() =>{signInWithGoogle()}}> Login </button>
             <h1>{localStorage.getItem("name")}</h1>
             <img src = {localStorage.getItem("photoURL")} />
         </div>
